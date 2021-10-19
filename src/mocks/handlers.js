839a1796aadcb53fe1e983fec104fdb2a405547c
Daniele <irsooti@gmail.com>
2021-10-19 15:02:10 +0200

    chore: create exercise

---
+++ src/mocks/handlers.js
@@ -0,0 +1,28 @@
+import { rest } from 'msw';
+
+const products = [
+  {
+    price: 2.5,
+    name: 'Pera',
+  },
+  {
+    price: 3.5,
+    name: 'Mela',
+  },
+  {
+    price: 3.9,
+    name: 'Banana',
+  },
+];
+
+export const handlers = [
+  rest.get('/products', (req, res, ctx) => {
+    res(ctx.status(200));
+    return res(ctx.json(products));
+  }),
+
+  rest.post('/buy', (req, res, ctx) => {
+    res(ctx.status(202));
+    return res(ctx.json(JSON.parse(req.body)));
+  }),
+];
